ef26e49022ae831cff9b47ea1067025451927746
BarracudaPff <mrm.kikll@gmail.com>
2018-12-15 19:15:46 +0300

    init 2

--- app/src/main/java/com/samsung/barracudapff/myapplication/MainActivity.java
+++ app/src/main/java/com/samsung/barracudapff/myapplication/MainActivity.java
@@ -2,6 +2,14 @@ package com.samsung.barracudapff.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +18,40 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView view = findViewById(R.id.rec);
+        view.setHasFixedSize(true);
+        view.set
+        view.setAdapter(new RecyclerView.Adapter() {
+            ArrayList<RecyclerView.ViewHolder> list = new ArrayList<>();
+
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        });
+
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        CheckBox checkBox;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.text);
+            checkBox = itemView.findViewById(R.id.button);
+        }
     }
 
 }
