e8fb556c743bcf53e330882b858186a5ca733188
BarracudaPff <mrm.kikll@gmail.com>
2018-12-15 19:51:40 +0300

    1

--- app/src/main/java/com/samsung/barracudapff/myapplication/MainActivity.java
+++ app/src/main/java/com/samsung/barracudapff/myapplication/MainActivity.java
@@ -3,7 +3,10 @@ package com.samsung.barracudapff.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -18,26 +21,39 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView view = findViewById(R.id.rec);
+        final RecyclerView view = findViewById(R.id.rec);
         view.setHasFixedSize(true);
-        view.set
-        view.setAdapter(new RecyclerView.Adapter() {
-            ArrayList<RecyclerView.ViewHolder> list = new ArrayList<>();
+        //линейная ориентация списка
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(new RecyclerView.Adapter<MyViewHolder>() {
+            public int count = 0;
 
             @NonNull
             @Override
-            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-                return null;
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View view = LayoutInflater
+                        .from(viewGroup.getContext())
+                        .inflate(R.layout.item_note, viewGroup, false);
+                return new MyViewHolder(view);
             }
 
             @Override
-            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+            public void onBindViewHolder(@NonNull MyViewHolder viewHolder, int i) {
 
             }
 
             @Override
             public int getItemCount() {
-                return 0;
+                return count;
+            }
+        });
+
+        FloatingActionButton actionButton = findViewById(R.id.floatingActionButton);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.getAdapter().notifyItemInserted(
+                        view.getAdapter().getItemCount()+1);
             }
         });
 
